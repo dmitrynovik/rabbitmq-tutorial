91b2b76d35c31342657c2c1e554f9b01477c8c47
dmitrtynovik <cryinstone@gmail.com>
2024-01-09 16:54:49 +1100

    feat: publisher confirms

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 
 A minimum set of 2 RabbitMQ .NET Clients (the `Send` and the `Receive`) demoing the:
 * Consumer acknowledgements
+* Publisher confirms
 * Connection and channels [automatic recovery](https://www.rabbitmq.com/dotnet-api-guide.html#recovery)
--- Send/Send.cs
+++ Send/Send.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Collections.Concurrent;
+using System.Text;
 using RabbitMQ.Client;
 
 var factory = new ConnectionFactory { HostName = "localhost", AutomaticRecoveryEnabled = true };
@@ -14,20 +15,51 @@ channel.QueueDeclare(queue: "hello",
                      autoDelete: false,
                      arguments: null /* TODO: Quorum */);
 
+// Configure to use publisher confirms:
+channel.ConfirmSelect();
+var outstandingConfirms = new ConcurrentQueue<ulong>();
+channel.BasicAcks += (sender, ea) => CleanOutstandingConfirms(outstandingConfirms, ea.DeliveryTag, ea.Multiple);
+channel.BasicNacks += (sender, ea) => CleanOutstandingConfirms(outstandingConfirms, ea.DeliveryTag, ea.Multiple);
+
 ConsoleKeyInfo key;
 do {
     string message = $"Hello {++count}!";
     var body = Encoding.UTF8.GetBytes(message);
 
+    var sequenceNumber = channel.NextPublishSeqNo;
+    outstandingConfirms.Enqueue(sequenceNumber);
     channel.BasicPublish(exchange: string.Empty,
                      routingKey: "hello",
                      basicProperties: null,
                      body: body);
+
+    // This is very inefficient:              
+    //channel.WaitForConfirmsOrDie(TimeSpan.FromSeconds(5));
 
     Console.WriteLine($" [x] Sent: {message}");
     Console.WriteLine(" Press [enter] to exit or any other key to continue.");
     key = Console.ReadKey();
     
 } while (key.Key != ConsoleKey.Enter);
+
+void CleanOutstandingConfirms(ConcurrentQueue<ulong> outstandingConfirms, ulong sequenceNumber, bool multiple)
+{
+    if (multiple)
+    {
+        for (;;) 
+        {
+            outstandingConfirms.TryPeek(out var tag);
+            if (tag > sequenceNumber)
+                break;
+            else
+                outstandingConfirms.TryDequeue(out _);
+        }
+    }
+    else
+    {
+        outstandingConfirms.TryDequeue(out _);
+    }
+    Console.WriteLine("Awaiting confirms: {0} messages (multiple: {1})", outstandingConfirms.Count, multiple);
+}
 
 
